8ce8ff430efa26c9edda0c93b30d4a0e6cc8e4c2
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-02-22 17:13:01 +0100

    refactor spine asset loading and animation handling in startLabel

--- src/labels/startLabel.ts
+++ src/labels/startLabel.ts
@@ -3,19 +3,43 @@ import { Spine } from "@drincs/pixi-vn-spine";
 
 const startLabel = newLabel("start", [
     async () => {
-        console.log((window as any).PIXI);
-        narration.dialogue = { text: `Spine Test` };
-    },
-    async () => {
-        await Assets.loadBundle("spine");
-        const spine = new Spine({ atlas: "spineAtlas", skeleton: "spineSkeleton" });
-        spine.x = canvas.width / 2;
-        spine.y = canvas.height;
+        await Assets.load(["spineboySkeleton", "spineboyAtlas"]);
+        const spine = new Spine({
+            skeleton: "spineboySkeleton",
+            atlas: "spineboyAtlas",
+        });
+        spine.align = { x: 0.5, y: 1 };
         canvas.add("spine", spine);
+        spine.addAnimation("idle", {
+            loop: true,
+            completeOnContinue: false,
+        });
+        spine.addAnimation("jump", {
+            delay: 0.5,
+        });
     },
     async () => {
         const spine = canvas.find<Spine>("spine");
-        spine?.state.setAnimation(0, "idle", true);
+        spine?.playSequence(
+            [
+                [
+                    "jump",
+                    {
+                        duration: 5,
+                    },
+                ],
+                [
+                    "walk",
+                    {
+                        duration: 9,
+                    },
+                ],
+            ],
+            {
+                repeat: 3,
+                repeatType: "loop",
+            },
+        );
         narration.dialogue = { text: `Spine animation should be playing` };
     },
     async () => {
