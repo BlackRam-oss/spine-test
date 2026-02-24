a929ce60e1feb74135a9f4945fdaf32df3b97ed1
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-02-24 17:21:57 +0100

    refactor startLabel to streamline spine animation setup and remove unused code

--- src/labels/startLabel.ts
+++ src/labels/startLabel.ts
@@ -1,25 +1,42 @@
-import { Assets, canvas, narration, newLabel } from "@drincs/pixi-vn";
+import { Assets, canvas, newLabel } from "@drincs/pixi-vn";
 import { Spine } from "@drincs/pixi-vn-spine";
 
 const startLabel = newLabel("start", [
-    async () => {
-        console.log((window as any).PIXI);
-        narration.dialogue = { text: `Spine Test` };
-    },
+    // async () => {
+    //     await Assets.loadBundle("spine");
+    //     const spine = new Spine({ atlas: "spineAtlas", skeleton: "spineSkeleton", xAlign: 0.5, yAlign: 1 });
+    //     spine.addAnimation("idle", { loop: true });
+    //     canvas.add("spine", spine);
+    // },
+    // async () => {
+    //     await Assets.loadBundle("spine");
+    //     const spine = new Spine({ atlas: "spineAtlas", skeleton: "spineSkeleton", xAlign: 0, yAlign: 1 });
+    //     spine.addAnimation("walk", { loop: true });
+    //     canvas.add("spine", spine);
+    //     canvas.animate(
+    //         spine,
+    //         [
+    //             [{ xAlign: 1 }, { duration: 1, ease: "linear" }],
+    //             [{ scaleX: -1 }, { duration: 0.2 }],
+    //             [{ xAlign: 0 }, { duration: 1, ease: "linear" }],
+    //             [{ scaleX: 1 }, { duration: 0.2 }],
+    //         ],
+    //         { repeat: Infinity },
+    //     );
+    // },
     async () => {
         await Assets.loadBundle("spine");
-        const spine = new Spine({ atlas: "spineAtlas", skeleton: "spineSkeleton" });
-        spine.x = canvas.width / 2;
-        spine.y = canvas.height;
+        const spine = new Spine({ atlas: "spineAtlas", skeleton: "spineSkeleton", xAlign: 0.5, yAlign: 1 });
+        spine.playSequence(
+            [
+                ["idle", { loop: true, duration: 2 }],
+                ["jump", { duration: 1 }],
+            ],
+            {
+                repeat: Infinity,
+            },
+        );
         canvas.add("spine", spine);
-    },
-    async () => {
-        const spine = canvas.find<Spine>("spine");
-        spine?.state.setAnimation(0, "idle", true);
-        narration.dialogue = { text: `Spine animation should be playing` };
-    },
-    async () => {
-        narration.dialogue = { text: `End` };
     },
 ]);
 export default startLabel;
